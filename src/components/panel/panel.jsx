8cab443b10a0607341f9a0a2719b589c3f7960db
gilson-delgado <gcdelgaa@gmail.com>
2024-07-10 23:01:29 -0300

    subindo arquivos para o gitHub

---
+++ src/components/panel/panel.jsx
@@ -0,0 +1,28 @@
+import { Component } from 'react'
+import './panel.css'
+
+class Panel extends Component{
+    constructor(){
+        super()
+        this.state = {
+            title: 'Título do painel'
+        }
+    }
+
+    render(){   
+          const thisComponent = this
+        return(
+            <section className="panel" onClick={
+            function () {              
+                thisComponent.setState(
+                        {title: 'Título novo'}
+                    )
+                }
+            }>
+                <h2>{this.state.title}</h2>
+            </section>
+        )
+    }
+}
+
+export default Panel
